7939726ee8d18c1761c6a428ea3806a9a1f8fe1c
karanraul02@gmail.com <karanraul02@gmail.com>
2024-01-26 18:34:57 +0530

    frontend

---
+++ smart_contract/scripts/deploy.js
@@ -0,0 +1,39 @@
+
+
+const main = async () => {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const unlockTime = currentTimestampInSeconds + 60;
+
+  const lockedAmount = hre.ethers.parseEther("0.001");
+
+  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
+    value: lockedAmount,
+  });
+
+  await lock.waitForDeployment();
+
+  console.log(
+    `Lock with ${ethers.formatEther(
+      lockedAmount
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  );
+}
+
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+
+    console.error(error);
+    process.exit(1);
+
+  }
+}
+
+runMain();
